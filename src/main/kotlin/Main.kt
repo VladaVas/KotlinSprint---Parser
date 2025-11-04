4776350e4af8b7b4b223ba1f83173d20e7b01aa1
Vlada Vasileva <vvavlada@gmail.com>
2025-11-04 19:04:44 +0300

    Решение задачи по парсингу сайти с цитатами Адама Дугласа

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,19 @@
 package org.example
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+
 fun main() {
-    println("Hello World!")
+
+    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
+    // println(doc.title())
+
+    val quotes: Elements = doc.select("div.sc-2aegk7-2.bzpNIu")
+    // println(quotes)
+
+    for (quote: Element in quotes) {
+        println("— ${quote.text()}\n")
+   }
 }
